1d351d6a7efff9ad800ccb4d242b73b8088d3585
Jacob Leazott <jacob.leazott@gmail.com>
2025-05-15 19:24:19 -0600

    Fixed js

--- js/script.js
+++ js/script.js
@@ -178,22 +178,16 @@ async function loadGalleryGroups() {
     return new Promise(requestAnimationFrame);
 }
 
-function showGallery(tag, images, pushState = true) {
-    if (pushState) {
-        history.pushState({ type: 'gallery', tag }, '', `#gallery-${tag}`);
-    }
-
-    const selector = document.getElementById('gallery-selector');
+function showGallery(tag, images) {
+    const selector         = document.getElementById('gallery-selector');
     const galleryContainer = document.getElementById('active-gallery-container');
-    const galleryDiv = document.getElementById('active-gallery');
-    const titleEl = document.getElementById('gallery-title');
-    const descEl = document.getElementById('gallery-description');
+    const galleryDiv       = document.getElementById('active-gallery');
 
-    document.getElementById('gallery-title').textContent = galleryData[tag].title;
+    document.getElementById('gallery-title').textContent       = galleryData[tag].title;
     document.getElementById('gallery-description').textContent = galleryData[tag].description;
-    document.getElementById('gallery-location').textContent = galleryData[tag].location;
-    document.getElementById('gallery-date').textContent = galleryData[tag].date;
-    document.getElementById('gallery-gear').textContent = galleryData[tag].gear;
+    document.getElementById('gallery-location').textContent    = galleryData[tag].location;
+    document.getElementById('gallery-date').textContent        = galleryData[tag].date;
+    document.getElementById('gallery-gear').textContent        = galleryData[tag].gear;
 
     galleryDiv.innerHTML = '';
     galleryDiv.dataset.tag = tag;
